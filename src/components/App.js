d0615023478da5de7fdc589a45aa36face90c916
VictorParodi <victor.parodi.montero@gmail.com>
2019-10-24 22:34:20 -0500

    app1 commit 4: making the resources request

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React, {useState} from 'react';
 import ResourceList from './ResourceList.js';
 
 const App = () => {
-    const [resource, setResource] = useState('POST');
+    const [resource, setResource] = useState('posts');
 
     return (
         <div className="ui container">
             <h1>App Component</h1>
             <div>
-                <button onClick={() => setResource('POST')} className="ui basic button">POSTS</button>
-                <button onClick={() => setResource('TODOS')} className="ui basic button">TODOS</button>
+                <button onClick={() => setResource('posts')} className="ui basic button">POSTS</button>
+                <button onClick={() => setResource('todos')} className="ui basic button">TODOS</button>
             </div>
             <ResourceList resource={resource}/>
         </div>
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+    state = {resource: []};
+
+    async componentDidMount() {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+        this.setState({resource: response});
+    }
+
+    async componentDidUpdate(prevProps) {
+        if (prevProps.resource !== this.props.resource) {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+            this.setState({resource: response});
+        }
+    }
+
     render() {
         return (
             <div>
